5acdb6798a89cbb0c3485b6c659fe224ac880aab
Laura Furman <98051403+laurafurman@users.noreply.github.com>
2022-10-19 01:44:56 -0400

    Refactored CardForm, CardAdd, and CardEdit

--- src/Layout/Cards/CardAdd.js
+++ src/Layout/Cards/CardAdd.js
@@ -1,30 +1,95 @@
-import React, {useState} from "react";
-import { useParams } from "react-router-dom";
-import {createCard} from "./../../utils/api";
-import CardForm from "./CardForm"
+import React, { useEffect, useState } from "react";
+import { useParams, Link, useHistory } from "react-router-dom";
+import { readDeck, createCard } from "../../utils/api";
+import CardForm from "./CardForm";
 
 function CardAdd() {
+  const abortController = new AbortController();
+
+  const initialValue = {
+    name: "",
+    description: "",
+  };
+
+  const [deck, setDeck] = useState(initialValue);
   const { deckId } = useParams();
 
-  const initialFormState = {
-    front: "Front side of card",
-    back: "Back side of card",
-    deckId: Number(deckId)
+  useEffect(() => {
+    setDeck(initialValue);
+
+    async function currentDeck(id) {
+      try {
+        const current = await readDeck(id, abortController.signal);
+        setDeck({ ...current });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    currentDeck(deckId);
+
+    return () => abortController.abort();
+  }, [deckId]);
+
+  //we have the deck stored in deck variable
+
+  const initialState = {
+    front: "",
+    back: "",
+  };
+
+  const [formData, setFormData] = useState({ ...initialState });
+  const history = useHistory();
+
+  function changeHandler({ target }) {
+    setFormData({
+      ...formData,
+      [target.name]: target.value,
+    });
   }
 
-  const [content, setContent] = useState({ ...initialFormState });
-
-  async function handleSave(event) {
+  async function submitHandler(event) {
     event.preventDefault();
-    await createCard(content);
-    setContent(initialFormState);
+    await createCard(deckId, formData, abortController.signal);
+    history.go(0);
   }
 
   return (
     <div>
-      <CardForm formData={content} setFormData={setContent} handleSave={handleSave} />
+      <nav className="navbar navbar-light">
+        <div className="container-fluid bg-light rounded">
+          <nav aria-label="breadcrumb">
+            <ol className="breadcrumb pt-3">
+              <li className="breadcrumb-item">
+                <Link
+                  className="bi bi-house-door-fill text-decoration-none"
+                  to={"/"}
+                >
+                  {" "}
+                  Home
+                </Link>
+              </li>
+              <li className="breadcrumb-item">
+                <Link className="text-decoration-none" to={`/decks/${deckId}`}>
+                  {deck.name}
+                </Link>
+              </li>
+              <li className="breadcrumb-item active" aria-current="page">
+                Add Card
+              </li>
+            </ol>
+          </nav>
+        </div>
+      </nav>
+      <h1 className="pt-4">{deck.name}: Add Card</h1>
+      <CardForm
+        submitHandler={submitHandler}
+        formData={formData}
+        changeHandler={changeHandler}
+        deckId={deckId}
+      />
     </div>
-  )
+  );
 }
 
 export default CardAdd;
